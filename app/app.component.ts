3aa0f5db3d32d3929db374c39919b28ff71ce99f
Austin Minnon and Chris Kuiper <chris.kuiper@gmail.com>
2016-03-22 13:55:27 -0700

    add pipe to our task

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,32 @@
-import { Component } from 'angular2/core';
-
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 // defining annotation aka "decorator"
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
   <div class="container">
     <h1>To-Do List using Angular</h1>
-    <h3>{{ task.description }}</h3>
+    <task-list
+     [taskList]="tasks"
+     (onTaskSelect)="taskWasSelected($event)">
+     </task-list>
   </div>
   ` //uses backticks
 })
 
-
-
 export class AppComponent {
-  public done: = false;
-  constructor(public description: string, public id: number) {
-
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung FU.", 1),
+      new Task("Rewatch all the lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log('parent', clickedTask);
   }
 }
